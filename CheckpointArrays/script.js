0efea859f199115e5e010f2fdc7655dcbe9704cd
Gustavo12 <gaortizpr@gmail.com>
2020-03-30 11:16:26 -0400

    checkpoint array

--- CheckpointArrays/script.js
+++ CheckpointArrays/script.js
@@ -1,5 +1,6 @@
 var drinks = ['Water', 'Coffee', 'Tea'];
 var foods = ['Apple', 'Sandwich', 'Chips'];
 var meals = drinks.concat(foods);
+var mealsReversed = meals.reverse();
 
-meals.reverse();
+console.log(mealsReversed);
