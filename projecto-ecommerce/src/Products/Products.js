cdc7e5d90ffa500fc48be5f92921cd14d9a22d06
Gustavo Ortiz <gustavoortiz@Gustavos-Air.attlocal.net>
2020-05-21 17:53:46 -0400

    ecommerce v2

---
+++ projecto-ecommerce/src/Products/Products.js
@@ -0,0 +1,119 @@
+import React from 'react';
+
+class Products extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                    <header className="header">
+                        <img className="logo" src="Fotos/FullColor_TransparentBg_1280x1024_72dpi.png" alt="logo" /><a href=""></a>
+                        <ul className="nav">
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="#">Products</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </header>
+                    <h1 className="JDM">Japanese Imports</h1>
+                    <div className="parent">
+                        <article className="child">
+                            <img className="child--image" src="Fotos/crestajdm.jpg" alt="child--image" />
+                            <h3><a href="#">Toyota</a></h3>
+                            <h4> 1997 Chaser Jzx-100</h4>
+                            <p>1JZ-GTE (2.5L 6-cylinder turbo, 5-speed manual)</p>
+                            <h5>Offer: $22,000</h5>
+                        </article>
+                        <article className="child">
+                            <img className="child--image" src="Fotos/r32jmd.jpg" alt="child--image" />
+                            <h3><a href="#"> Nissan Skyline</a></h3>
+                            <h4> 1993 R32 GTR </h4>
+                            <p>RB26DETT (2.6L 6-cylinder turbo, 5-speed manual)</p>
+                            <h5>Offer: $36,000</h5>
+                        </article>
+                        <article className="child">
+                            <img className="child--image" src="Fotos/r33jdm.jpg" alt="child--image" />
+                            <h3><a href="#"> Nissan Skyline</a></h3>
+                            <h4> 1996 R33 GTR </h4>
+                            <p>RB26DETT (2.6L 6-cylinder turbo, 5-speed manual)</p>
+                            <h5>Offer: $42,000</h5>
+                        </article>
+                        <article className="child">
+                            <img className="child--image" src="Fotos/R34jdm.jpg" alt="child--image" />
+                            <h3><a href="#" >Nissan Skyline</a></h3>
+                            <h4> 1998 R34 GTR </h4>
+                            <p>RB26DETT (2.6L 6-cylinder turbo, 5-speed manual)</p>
+                            <h5>Offer: $65,000</h5>
+                        </article>
+                        <article className="child">
+                            <img className="child--image" src="Fotos/r35jdm.jpg" alt="child--image" />
+                            <h3><a href="#">Nissan</a></h3>
+                            <h4> 2017 GTR </h4>
+                            <p>VR38DETT (3.8-liter V-6 twin-turbo, dual-clutch) </p>
+                            <h5>Offer: $60,0000</h5>
+                        </article>
+                        <article className="child">
+                            <img className="child--image" src="Fotos/s15.2jdm.jpg" alt="child--image" />
+                            <h3><a href="#">Nissan</a></h3>
+                            <h4> 1999 Sylvia S15 </h4>
+                            <p>SR20DET (2.0L 4-cylinder turbo, 5-speed manual)</p>
+                            <h5>Offer: $25,000</h5>
+                        </article>
+                        <article className="child">
+                            <img className="child--image" src="Fotos/fcjdm.jpg" alt="child--image" />
+                            <h3><a href="#">Mazda</a></h3>
+                            <h4> 1988 Rx-7 (Fc) </h4>
+                            <p>13B Rotary (1.3L turbocharged, 5-speed manual)</p>
+                            <h5>Offer: $15,000</h5>
+                        </article>
+                        <article className="child">
+                            <img className="child--image" src="Fotos/Rx7jdm.png" alt="child--image" />
+                            <h3><a href="#" >Mazda</a></h3>
+                            <h4> 1998 Rx-7 (Fd)</h4>
+                            <p>13B Rotary (1.3L twin-turbocharged, 5-speed manual)</p>
+                            <h5>Offer: $40,0000</h5>
+                        </article>
+                        <article className="child">
+                            <img className="child--image" src="Fotos/nsxjdm.jpg" alt="child--image" />
+                            <h3><a href="#"> Honda </a></h3>
+                            <h4>1991 NSX </h4>
+                            <p>C32B (3.2L V-6, 5-speed manual)</p>
+                            <h5> Offer: $50,000</h5>
+                        </article>
+                        <article className="child">
+                            <img className="child--image" src="Fotos/suprajdm.jpg" alt="child--image" />
+                            <h3><a href="#" >Toyota </a></h3>
+                            <h4> 1996 Supra Mk-IV </h4>
+                            <p>2JZ-GTE (2.9L 6-cylinder twin-turbocharged, 6-speed manual)</p>
+                            <h5>Offer: $45,000</h5>
+                        </article>
+                        <article className="child">
+                            <img className="child--image" src="Fotos/jzx90jdm.jpg" alt="child--image" />
+                            <h3><a href="#" > Toyota</a></h3>
+                            <h4> 1995 Chaser Jzx-90</h4>
+                            <p>1JZ-GTE (2.5L 6-cylinder, 5-speed manual)</p>
+                            <h5>Offer: $18,000</h5>
+                        </article>
+                        <article className="child">
+                            <img className="child--image" src="Fotos/240jdm.jpg" alt="child--image" />
+                            <h3><a href="#" >Nissan</a></h3>
+                            <h4> 1993 S13 240sx </h4>
+                            <p> KA24DE (2.4L 4-cylinder, 5-speed manual)</p>
+                            <h5>Offer: $20,000</h5>
+                        </article>
+                    </div>
+                    <footer className="footer1">
+                        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
+                        <ul className="ul-2">
+                            <li><a className="facebook"><i className="fa fa-fw fa-2x fa-inverse fa-facebook"></i></a></li>
+                            <li><a className="twitter"><i className="fa fa-fw fa-2x fa-inverse fa-twitter"></i></a></li>
+                            <li><a className="instagram"><i className="fa fa-fw fa-2x fa-inverse fa-instagram"></i></a></li>
+                        </ul>
+                        <a className="a" href="#" id="footer">
+                            <h3> JDM</h3>
+                        </a>
+                        <h3>&copy;2020 | Gustavo A. Ortiz</h3>
+                    </footer>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Products;
